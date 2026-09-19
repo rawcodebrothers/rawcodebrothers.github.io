c036a64c4bfb4f35611be54c3cfa247929e72c0d
Simão Júnior <simao.msjr@gmail.com>
2026-09-19 00:56:34 -0300

    chore(astro): point the site url at the renamed organization
    Signed-off-by: Simão Júnior <simao.msjr@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rawcodelabs.github.io",
+  site: "https://rawcodebrothers.github.io",
 
   vite: {
     plugins: [tailwindcss()],
